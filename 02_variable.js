a79a591c75c1b00e94e4ba8d5083ece7f7ee16c4
AIS09-sys <mynameisalish1@gmail.com>
2026-01-16 10:36:17 +0545

    Add files via upload

---
+++ 02_variable.js
@@ -0,0 +1,8 @@
+// var → function scoped
+// It works only inside the function where it is declared
+
+// let → block scoped
+// It works only inside the block { }
+
+// const → block scoped
+// Its value cannot be changed after declaration
