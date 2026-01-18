3ba6baad91858b562a6c8b9c60309f88330f7b52
AIS09-sys <mynameisalish1@gmail.com>
2026-01-18 12:01:40 +0545

    Modify age and add console logs for data types
    Updated age variable and added console logs for various data types.

--- 03_datatypes.js
+++ 03_datatypes.js
@@ -1,8 +1,16 @@
 let name = "ALISH";      // String
-let age = 21;          // Number
+let age = 19;          // Number
 let isAdmin = true;    // Boolean
 let x;                 // Undefined
 let y = null;          // Null
 let bigNum = 12345678901234567890n; // BigInt
 let id = Symbol("id"); // Symbol
+console.log(name);
+console.log(age);
+console.log(isBolean);
+console.log(x);
+console.log(y);
+console.log(bigNum);
+console.log(id.description);
+
 
