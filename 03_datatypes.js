d7711930358fbf91dad1795bde6b63fdee3f64c9
AIS09-sys <mynameisalish1@gmail.com>
2026-01-18 11:41:31 +0545

    Add files via upload

---
+++ 03_datatypes.js
@@ -0,0 +1,7 @@
+let name = "AlS";      // String
+let age = 21;          // Number
+let isAdmin = true;    // Boolean
+let x;                 // Undefined
+let y = null;          // Null
+let bigNum = 12345678901234567890n; // BigInt
+let id = Symbol("id"); // Symbol
