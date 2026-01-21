d819f7b1db9c82e9389d058ca92971b0fbbac1d4
AIS09-sys <mynameisalish1@gmail.com>
2026-01-21 07:11:05 +0545

    Add files via upload

---
+++ 05_conditions.js
@@ -0,0 +1,12 @@
+const age = 18;
+
+/* if-else */
+if (age >= 18) {
+  console.log("Adult");
+} else {
+  console.log("Minor");
+}
+
+/* ternary */
+const result = age >= 18 ? "Adult" : "Minor";
+console.log(result);
